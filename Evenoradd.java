20c136b0a72107917ab88a1269911304038d86ce
unknown <pigilapuchaithanya123@gmail.com>
2024-07-11 16:23:56 +0530

    core java programs

---
+++ Evenoradd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class evenorodd
+{
+    public static void main(string args[])
+    {
+    scanner sc=new scanner(system.in);
+    intn=2;
+    system.out.println("Enter the value of n");
+    n=sc.nextInt();
+    if(n%2==0)
+    {
+      system.out.println("given number is even");
+    }
+   else
+    {
+      system.out.println("given number is odd");
+    }
+}
